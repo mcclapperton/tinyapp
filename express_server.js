f9df4de1a2e60423e11b2558534acad2f0b7e739
Maris <mcclapperton@gmail.com>
2022-10-31 21:37:38 -0600

    renders done, results match assignment screenshots, woo

--- express_server.js
+++ express_server.js
@@ -12,6 +12,14 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+//url_show page
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
