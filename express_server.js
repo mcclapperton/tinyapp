d298506c685f541ed7b77e4f149e0bbb33108ceb
Maris <mcclapperton@gmail.com>
2022-11-01 08:24:18 -0600

    have now added generateRandomString functionality and corrected redirect in app.post to  /urls/:id

--- express_server.js
+++ express_server.js
@@ -4,21 +4,34 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-function generateRandomString() {}
+//generates a 6 digit random string for id
+const generateRandomString = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let randomId = [];
+  let splitArray = characters.split("");
+  for (let i = 0; i <= 5; i++) {
+    randomId.push(splitArray[Math.floor(Math.random() * characters.length)]);
+  }
+  return randomId.join("");
+};
 
 //keeps track of all the urls and their shortened forms
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+//routes
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 app.post("/urls", (req, res) => {
-  urlDatabase.id = req.body.longURL; // id-longURL key value pair save to urlDatabase
-  console.log(urlDatabase);
-  res.redirect("/urls"); // need to redirect to /urls/:id
+  let newId = generateRandomString();
+  urlDatabase[newId] = req.body.longURL; // id-longURL key value pair save to urlDatabase
+  // console.log(urlDatabase);
+  res.redirect(`/urls/${newId}`); // need to redirect to /urls/:id
 });
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -31,7 +44,11 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
+//redirect to longURl when click id
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
